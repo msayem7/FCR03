774398a9e777020bbf27ce8ed7fe72fc31bda66a
“msayem7” <msayem7@gmail.com>
2025-02-26 19:07:35 +0600

    Test1

---
+++ src/stores/a.js
@@ -0,0 +1,28 @@
+
+import { defineStore } from 'pinia';
+import axios from '../plugins/axios';
+import { useAuthStore } from './authStore';
+
+export const useBranchStore = defineStore('credit', {   
+    state: () => ({
+        selectedBranch: localStorage.getItem('workingBranch') || null,        
+        branches: [],        
+        refreshTrigger: 0,
+    }),
+    getters: {
+        isBranchSelected: (state) => state.selectedBranch !== null,
+    },
+    actions: {
+        async loadBranches() {
+            const authStore = useAuthStore();
+            if (!authStore.user) return;
+            const { data } = await axios.get('/branches/');
+            this.branches = data;
+        },
+        setWorkingBranch(branch) {
+            this.selectedBranch = branch;
+            localStorage.setItem('workingBranch', branch);
+            this.refreshTrigger++; // Increment the refresh trigger
+        },
+    },
+});
