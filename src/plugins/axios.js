1f93a5de4a20715034583cb4183b75c62e865f23
“msayem7” <msayem7@gmail.com>
2025-03-06 12:19:43 +0600

    Add environment variable for base URL

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,8 +3,11 @@ import axios from 'axios';
 // axios.defaults.baseURL = 'http://localhost:8000/api/';
 // axios.defaults.headers.common['Authorization'] = localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : '';
 
+console.log(`process.env.VUE_APP_API_BASE_URL: ${process.env.VUE_APP_API_BASE_URL}`)
+console.log(`process.env.VUE_APP_API_BASE_URL: ${process.env}`)
 const instance = axios.create({
-  baseURL:'http://localhost:8000/v1/chq',
+  //baseURL:'http://localhost:8000/v1/chq',
+  baseURL: `${process.env.VUE_APP_API_BASE_URL}/v1/chq`,
 })
 
 instance.interceptors.request.use((config)=>{
