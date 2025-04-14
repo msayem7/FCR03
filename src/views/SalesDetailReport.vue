c211ce112a4f15106a59ee82a5b3dbef7ac4c221
“msayem7” <msayem7@gmail.com>
2025-04-14 12:09:11 +0600

    fine tune

--- src/views/SalesDetailReport.vue
+++ src/views/SalesDetailReport.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <h1>Single Sales Report full details, all deduction, cheque's status and history </h1>
-    </div>
+  <div>
+    <h1>Single Sales Report full details, all deduction, cheque's status and history </h1>
+  </div>
 </template>
     
 <script setup>
